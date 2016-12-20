2a895dada135332febb689db2c5e7ca7afb86279
Erik Paldanius <erik.paldanius@gmail.com>
2016-12-20 17:53:43 +0100

    made the no audio default audio module not return null when loading stuff but instead a null object

--- Cog2D/Modules/Audio/DefaultAudioModule.cs
+++ Cog2D/Modules/Audio/DefaultAudioModule.cs
@@ -6,16 +6,65 @@ using System.Threading.Tasks;
 
 namespace Cog.Modules.Audio
 {
+    public class NoSoundInstance : ISoundInstance
+    {
+        public SoundEffect sfx;
+
+        public bool Paused
+        {
+            get; set;
+        }
+
+        public SoundEffect SoundEffect
+        {
+            get
+            {
+                return sfx;
+            }
+        }
+
+        public float Volume
+        {
+            get; set;
+        }
+
+        public void Stop()
+        {
+        }
+    }
+
+    public class NoSoundSoundEffect : SoundEffect
+    {
+        public override ISoundInstance CreateInstance()
+        {
+            return new NoSoundInstance { sfx = this };
+        }
+
+        public override void Dispose()
+        {
+        }
+
+        public override ISoundInstance Loop()
+        {
+            return new NoSoundInstance { sfx = this };
+        }
+
+        public override ISoundInstance Play()
+        {
+            return new NoSoundInstance { sfx = this };
+        }
+    }
+
     public class DefaultAudioModule : IAudioModule
     {
         public SoundEffect Load(byte[] data)
         {
-            return null;
+            return new NoSoundSoundEffect();
         }
 
         public SoundEffect Load(string file)
         {
-            return null;
+            return new NoSoundSoundEffect();
         }
     }
 }
